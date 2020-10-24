2c0bb9c45c744d4d89979eb91b450e127a8c4a3b
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 06:18:05 +0530

    Performing second Arithmetic operation

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -4,5 +4,7 @@ read -p "enter 1st number" a
 read -p "enter 1st number" b
 read -p "enter 1st number" c
 
-oprt1=$(( $a + $b *$c ))
+oprt1=$(( $a + $b * $c ))
 echo  "arithmatic operation1 ="$oprt1
+oprt2=$(( $a * $b + $c ))
+echo  "arithmatic operation2 ="$oprt2
