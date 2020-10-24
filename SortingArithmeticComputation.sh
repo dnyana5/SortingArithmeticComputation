d0ab250fd06046a0950dab0723b8a058d8c1968a
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 06:31:36 +0530

    Result stored in dictionary form

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+declare -A whole_operation
+
 read -p "enter 1st number" a
 read -p "enter 1st number" b
 read -p "enter 1st number" c
 
-oprt1=$(( $a + $b * $c ))
-echo  "arithmatic operation1 ="$oprt1
-oprt2=$(( $a * $b + $c ))
-echo  "arithmatic operation2 ="$oprt2
-oprt3=$(( $C + $a / $b ))
-echo  "arithmatic operation3 ="$oprt3
+oprt[1]=$((a + b * c))
+#echo  "arithmatic operation1 ="$oprt
+oprt[2]=$((a * b + c))
+#echo  "arithmatic operation2 ="$oprt2
+oprt[3]=$((c + (a / b)))
+#echo  "arithmatic operation3 ="$oprt3
+oprt[4]=$((a % b + c))
+#echo  "arithmatic operation4 ="$oprt4
+
+echo "whole_operation=" ${oprt[@]}
