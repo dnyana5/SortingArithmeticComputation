a264884bca0c163328cd62374f3da42f2d5d3ff8
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 06:02:11 +0530

    for different arithmetic operations Taking inputs from user

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Welcome to the Sorting Arithmetic Computation Program on master Branch"
+read -p "enter 1st number" a
+read -p "enter 1st number" b
+read -p "enter 1st number" c
+
