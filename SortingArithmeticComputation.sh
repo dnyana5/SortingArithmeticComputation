1ca0ae521c4ccbeb15f93ff9dd3223079811d9e6
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 06:51:53 +0530

    Resul sorted in asending order

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 declare -A whole_operation
 declare -a Array_operation
 read -p "enter 1st number" a
@@ -13,10 +14,28 @@ oprt[2]=$((a * b + c))
 oprt[3]=$((c + (a / b)))
 oprt[4]=$((a % b + c))
 
-echo "whole_operation=" ${oprt[@]}
+echo "whole_operation=" ${oprt[@]} | sort -n
 while [ $num -lt 5 ]
 do
-   Array_operation[$num]=${oprt[$num]}
+Array_operation[$num]=${oprt[$num]}
    ((num++))
 done
 echo "Array_operation=" ${Array_operation[@]}
+
+for ((i=0; i < ${#Array_operation[@]}; i++))
+do
+    for ((j=0 ; j < ${#Array_operation[@]}; j++))
+    do
+      if [[ ${Array_operation[$j]} -gt  ${Array_operation[$i]} ]]
+      then
+        tmp=${Array_operation[$i]}
+        Array_operation[$i]=${Array_operation[$j]}
+        Array_operation[$j]=${tmp}
+      fi
+    done
+done
+ echo "Asending order"
+for n in "${Array_operation[@]}"
+do
+      echo "$n"
+done
