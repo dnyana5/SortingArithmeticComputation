77319caf06eb49c32934f4a531102ae6895b68eb
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 06:45:34 +0530

    Result stored in Array form

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 declare -A whole_operation
-
+declare -a Array_operation
 read -p "enter 1st number" a
 read -p "enter 1st number" b
 read -p "enter 1st number" c
+num=1
+
 
 oprt[1]=$((a + b * c))
-#echo  "arithmatic operation1 ="$oprt
 oprt[2]=$((a * b + c))
-#echo  "arithmatic operation2 ="$oprt2
 oprt[3]=$((c + (a / b)))
-#echo  "arithmatic operation3 ="$oprt3
 oprt[4]=$((a % b + c))
-#echo  "arithmatic operation4 ="$oprt4
 
 echo "whole_operation=" ${oprt[@]}
+while [ $num -lt 5 ]
+do
+   Array_operation[$num]=${oprt[$num]}
+   ((num++))
+done
+echo "Array_operation=" ${Array_operation[@]}
